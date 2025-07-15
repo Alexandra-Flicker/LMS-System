648096c345c2bb2febed11dbf1320a219574a655
Alexandra Nygimetzhanova <alexsandra2600@gmail.com>
2025-07-15 23:59:23 +0500

    Removed rudiment

--- internal/usecase/service.go
+++ internal/usecase/service.go
@@ -1,8 +1,6 @@
 package usecase
 
 import (
-	"context"
-	"errors"
 	"lms_system/internal/domain"
 )
 
@@ -14,13 +12,4 @@ func NewService(mainRepo domain.MainRepositoryInterface) *Service {
 	return &Service{
 		mainRepo: mainRepo,
 	}
-}
-
-func (s *Service) Update(ctx context.Context, courseID uint) error {
-	// Validate
-	if courseID == 0 {
-		return errors.New("course id is required")
-	}
-
-	s.mainRepo.Course().CreateCourse(ctx, 33)
 }
